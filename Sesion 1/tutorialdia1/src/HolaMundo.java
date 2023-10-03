4e9027881e2c8ff22532ffda79430c8559f1789e
rdsbl <rdsbl@DESKTOP-L8S84KG>
2023-10-03 19:38:38 +0200

    Agregado sesion1

---
+++ Sesion 1/tutorialdia1/src/HolaMundo.java
@@ -0,0 +1,10 @@
+
+public class HolaMundo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hola CMEPPS");
+		System.out.println("bienvenido al curso 23/24");
+		System.out.print("Hoy hace sol.");
+	}
+}
