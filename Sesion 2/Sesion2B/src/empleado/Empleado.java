83026416b41cc7da721fab131f883406b86f4e79
rdsbl <rdsbl@DESKTOP-L8S84KG>
2023-10-07 20:59:13 +0200

    Pruebas de calculoNominaBruta

--- Sesion 2/Sesion2B/src/empleado/Empleado.java
+++ Sesion 2/Sesion2B/src/empleado/Empleado.java
@@ -5,10 +5,10 @@ public class Empleado {
 		Vendedor, Encargado
 	};
 	
-	public float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) {
+	public static float calculoNominaBruta(TipoEmpleado tipo, float ventasMes, float horasExtra) {
 		float salariobase = 0, primas = 0, extras = 0;
 		final int PRECIO_HE = 30;	
-		
+				
 		//determinacion salario base:
 		if(tipo == TipoEmpleado.Vendedor)
 			salariobase = 2000;
@@ -19,13 +19,19 @@ public class Empleado {
 			primas = 200;
 		else if (ventasMes >= 1000)
 			primas = 100;
+		else if(ventasMes < 0)
+			primas = 0;
 		//determinacion extras
+		if(horasExtra < 0)
+			horasExtra = 0;
 		extras = horasExtra*PRECIO_HE;
 				
 		return salariobase + primas + extras;
 	}
 	
-	public float calculoNominaNeta(float nominaBruta) {
+	public static float calculoNominaNeta(float nominaBruta) {
+		if(nominaBruta <0)
+			nominaBruta = 0;
 		float salarioNeto = 0, retencion = 0;
 		if(nominaBruta <= 2100)
 			retencion = 0;
@@ -36,4 +42,6 @@ public class Empleado {
 		salarioNeto = nominaBruta*(1-retencion);
 		return salarioNeto;
 	}
+	
+	
 }
--- Sesion 2/Sesion2B/src/test/EmpleadoTest.java
+++ Sesion 2/Sesion2B/src/test/EmpleadoTest.java
@@ -14,23 +14,101 @@ class EmpleadoTest {
 
 	@BeforeAll
 	static void setUpBeforeClass() throws Exception {
+		System.out.println("Before all");
 	}
 
 	@AfterAll
 	static void tearDownAfterClass() throws Exception {
+		System.out.println("After all");
 	}
 
 	@BeforeEach
 	void setUp() throws Exception {
+		System.out.println("Before each");
 	}
 
 	@AfterEach
 	void tearDown() throws Exception {
+		System.out.println("After each");
 	}
-
+	
+	/**
+	 * Prueba de un empleado vendedor con 0€ ventas al mes y 0 horas extra.
+	 */
 	@Test
-	void testCalculoNominaBruta() {
-		fail("Not yet implemented");
+	void testCalculoNominaBrutaVendedor() {
+		float expected = 2000;
+		float actual = Empleado.calculoNominaBruta(Empleado.TipoEmpleado.Vendedor, 0, 0);
+		assertEquals(expected, actual);
 	}
-
+	
+	/**
+	 * Prueba de un empleado encargado con 0€ ventas al mes y 0 horas extra.
+	 */
+	@Test
+	void testCalculoNominaBrutaEncargado() {
+		float expected = 2500;
+		float actual = Empleado.calculoNominaBruta(Empleado.TipoEmpleado.Encargado, 0, 0);
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Prueba de un empleado vendedor con 1345€ ventas al mes y 0 horas extra
+	 */
+	@Test
+	void testCalculoNominaBrutaVendedorPrima100() {
+		float expected = 2100;
+		float actual = Empleado.calculoNominaBruta(Empleado.TipoEmpleado.Vendedor, 1345, 0);
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Prueba de un empleado vendedor con 1501€ ventas al mes y 0 horas extra
+	 */
+	@Test
+	void testCalculoNominaBrutaVendedorPrima200() {
+		float expected = 2200;
+		float actual = Empleado.calculoNominaBruta(Empleado.TipoEmpleado.Vendedor, 1501, 0);
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Prueba de un empleado encargado con 1345€ ventas al mes y 0 horas extra
+	 */
+	@Test
+	void testCalculoNominaBrutaEncargadoPrima100() {
+		float expected = 2600;
+		float actual = Empleado.calculoNominaBruta(Empleado.TipoEmpleado.Vendedor, 1501, 0);
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Prueba de un empleado encargado con 1501€ ventas al mes y 0 horas extra
+	 */
+	@Test
+	void testCalculoNominaBrutaEncargadoPrima200() {
+		float expected = 2700;
+		float actual = Empleado.calculoNominaBruta(Empleado.TipoEmpleado.Encargado, 1501, 0);
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Prueba de un empleado vendedor con 1345€ ventas al mes y 30 horas extra
+	 */
+	@Test
+	void testCalculoNominaBrutaVendedorExtras() {
+		float expected = 2460;
+		float actual = Empleado.calculoNominaBruta(Empleado.TipoEmpleado.Vendedor, 1345, 12);
+		assertEquals(expected, actual);
+	}
+	
+	/**
+	 * Prueba de un empleado encargado con 2300€ ventas al mes y 17 horas extra
+	 */
+	@Test
+	void testCalculoNominaBrutaEncargadoExtra() {
+		float expected = 3210;
+		float actual = Empleado.calculoNominaBruta(Empleado.TipoEmpleado.Encargado, 2300, 17);
+		assertEquals(expected, actual);
+	}
 }
