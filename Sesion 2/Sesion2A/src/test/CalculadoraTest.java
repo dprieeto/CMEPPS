baefddafcaa0626b5a4c7f59787627b3c5f82647
rdsbl <rdsbl@DESKTOP-L8S84KG>
2023-10-08 18:09:29 +0200

    Documentacion de la clase de pruebas

--- Sesion 2/Sesion2A/src/test/CalculadoraTest.java
+++ Sesion 2/Sesion2A/src/test/CalculadoraTest.java
@@ -6,7 +6,9 @@ import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInfo;
 
 import cmepps.Calculadora;
 
@@ -39,35 +41,45 @@ class CalculadoraTest {
 	}
 
 	@Test
-	void testSuma() {
+	@DisplayName("Realiza la prueba de suma")
+	void testSuma(TestInfo info) {
+		System.out.println("Ejecutando prueba: " + info.getTestMethod().get().getName());
 		int expected =8;
 		int actual = Calculadora.suma(value1, value2);
 		assertEquals(expected, actual);		
 	}
 
 	@Test
-	void testResta() {
+	@DisplayName("Realiza la prueba de resta")
+	void testResta(TestInfo info) {
+		System.out.println("Ejecutando prueba: " + info.getTestMethod().get().getName());
 		int expected =2;
 		int actual = Calculadora.resta(value1, value2);
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	void testDivide() {
+	@DisplayName("Realiza la prueba de division")
+	void testDivide(TestInfo info) {
+		System.out.println("Ejecutando prueba: " + info.getTestMethod().get().getName());
 		int expected =1;
 		int actual = Calculadora.divide(value1, value2);
 		assertEquals(expected, actual);
 	}
 	
 	@Test
-	void testDividePor0() {
+	@DisplayName("Realiza la prueba de division por 0")
+	void testDividePor0(TestInfo info) {
+		System.out.println("Ejecutando prueba: " + info.getTestMethod().get().getName());
 		int expected =-1;
 		int actual = Calculadora.divide(value1, 0);
 		assertEquals(expected, actual);
 	}
 
 	@Test
-	void testMultiplica() {
+	@DisplayName("Realiza la prueba de multiplicacion")
+	void testMultiplica(TestInfo info) {
+		System.out.println("Ejecutando prueba: " + info.getTestMethod().get().getName());
 		int expected =15;
 		int actual = Calculadora.multiplica(value1, value2);
 		assertEquals(expected, actual);
