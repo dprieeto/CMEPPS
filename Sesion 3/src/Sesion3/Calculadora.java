9f620c6f1bba997e0ed6811c3c4b477603c6056b
usuario <usuario@pc20-019>
2023-10-10 10:27:41 +0200

    TDD Paso 3

--- Sesion 3/src/Sesion3/Calculadora.java
+++ Sesion 3/src/Sesion3/Calculadora.java
@@ -3,28 +3,22 @@ package Sesion3;
 public class Calculadora {
 	
 	public static int suma(int a, int b) {
-		return 5;
-		//return a+b;
+		return a+b;
 	}
 	
 	public static int resta(int a, int b) {
-		return -1;
-		//return a-b;//
+		return a-b;//
 	}
 	
 	
 	public static int divide(int a, int b) {
-		/*
 		if(b==0)
 			return -1;
 		else return a/b;
-		*/
-		return 6;
 	}
 	
 	public static int multiplica(int a, int b) {
-		return 0;
-		//return a*b;
+		return a*b;
 	}
 	
 }
