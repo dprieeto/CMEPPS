636dc5b719e3366e343a54033716c792be778013
rdsbl <rdsbl@DESKTOP-L8S84KG>
2023-10-12 17:19:44 +0200

    Version 1. Movimiento
    Creacion de la clase Movimiento, inicializacion de los atributos,
    constructor de la clase, getters y setters y documentacion de la clase.

---
+++ Sesion 3/Sesion 3/src/Sesion3/Movimiento.java
@@ -0,0 +1,70 @@
+/**
+ * 
+ */
+package Sesion3;
+
+/**
+ * Esta clase gestiona los movimientos de la cuenta del banco de un cliente.
+ * @author David Prieto Araujo
+ */
+public class Movimiento {
+
+	/**
+	 * Importe de la transaccion en la cuenta.
+	 */
+	private double importe;
+	/**
+	 * Tipo transaccion bancaria.
+	 * D es un cargo en la cuenta.
+	 * H un ingreso en la cuenta.
+	 */
+	public enum signo { // publica para que pueda usarse fuera de la clase.
+		D,
+		H;
+	}
+	
+	/**
+	 * Concepto de la retirada/ingreso del dinero.
+	 */
+	private String detalle;
+	
+	/**
+	 * Constructor de la clase. Inicializa los atributos de la clase.
+	 * @param importe
+	 * @param detalle
+	 * @param signo
+	 */
+	public Movimiento(double importe, String detalle, signo signo) {
+		super();
+		this.importe = importe;
+		this.detalle = detalle;
+	}
+
+	/**
+	 * @return the importe. Devuelve el importe de la transaccion.
+	 */
+	public double getImporte() {
+		return importe;
+	}
+
+	/**
+	 * @param importe. Añade el importe de la transaccion.
+	 */
+	public void setImporte(double importe) {
+		this.importe = importe;
+	}
+
+	/**
+	 * @return the detalle. Devuelve el concepto de la transaccion.
+	 */
+	public String getDetalle() {
+		return detalle;
+	}
+
+	/**
+	 * @param detalle. Añade el concepto de la transaccion.
+	 */
+	public void setDetalle(String detalle) {
+		this.detalle = detalle;
+	}	
+}
