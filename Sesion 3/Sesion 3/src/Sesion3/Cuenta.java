208005cfa27128bb8931b127897cf2bb96f03c8d
rdsbl <rdsbl@DESKTOP-L8S84KG>
2023-10-12 17:15:56 +0200

    Documentacion clase Cuenta

--- Sesion 3/Sesion 3/src/Sesion3/Cuenta.java
+++ Sesion 3/Sesion 3/src/Sesion3/Cuenta.java
@@ -1,7 +1,7 @@
 package Sesion3;
 
 /**
- * 
+ * Clase utilizada para la gestion de la cuenta de un cliente de un banco.
  * @author David Prieto Araujo
  */
 public class Cuenta {
@@ -23,51 +23,59 @@ public class Cuenta {
 	}
 
 	/**
-	 * @return the numero
+	 * @return el numero de cuenta.
 	 */
 	public String getNumero() {
 		return numero;
 	}
 
 	/**
-	 * @param numero the numero to set
+	 * @param numero. Modifica el numero de cuenta.
 	 */
 	public void setNumero(String numero) {
 		this.numero = numero;
 	}
 
 	/**
-	 * @return the titular
+	 * @return titular. Nombre del titular de la cuenta.
 	 */
 	public String getTitular() {
 		return titular;
 	}
 
 	/**
-	 * @param titular the titular to set
+	 * @param titular. Modifica el titular de la cuenta.
 	 */
 	public void setTitular(String titular) {
 		this.titular = titular;
 	}
 
 	/**
-	 * @return the saldo
+	 * @return saldo. Saldo actual de la cuenta.
 	 */
 	public double getSaldo() {
 		return saldo;
 	}
 
 	/**
-	 * @param saldo the saldo to set
+	 * @param saldo. Modifica el saldo actual de la cuenta.
 	 */
 	public void setSaldo(double saldo) {
 		this.saldo = saldo;
 	}
-
+	
+	/**
+	 * Ingresa dinero y actualiza la cuenta del cliente.
+	 * @param x
+	 */
 	public void ingresar(double x) {
 		this.setSaldo( x + this.getSaldo());
 	}
 	
+	/**
+	 * Retira dinero y actualiza la cuenta del cliente.
+	 * @param x
+	 */
 	public void retirar(double x) {
 		this.setSaldo(this.getSaldo() - x);
 	}
